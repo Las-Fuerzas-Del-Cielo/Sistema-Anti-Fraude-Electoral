736ebfa066a05045e76adf878255e06583c9abba
JuanFernigrini <juanfernigrini@gmail.com>
2023-10-28 16:00:45 -0300

    fiscalesdb

--- db-mysql/fiscales_db.sql
+++ db-mysql/fiscales_db.sql
@@ -14,40 +14,118 @@ SET time_zone = "+00:00";
 
 -- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `users`
---
-
-CREATE TABLE `users` (
-  `id` mediumint(8) UNSIGNED NOT NULL,
-  `username` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL COMMENT 'MD5',
-  `role` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
-  `enabled` tinyint(1) NOT NULL DEFAULT 0,
-  `creation_stamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `update_stamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+-- -------
+-- Estado de los usuarios 
+-- -------
+CREATE TABLE `gen_user_status` (
+  `id` int(11) NOT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tablas volcadas
---
+-- -------
+-- Niveles de usuarios para seguridad (alternativa a Rol)
+-- -------
+CREATE TABLE `gen_user_level` (
+  `id` int(11) NOT NULL,
+  `description` varchar(45) NOT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indices de la tabla `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+-- -------
+-- Usuarios, con firebaseId
+-- -------
+CREATE TABLE `gen_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `firebase_uid` varchar(255) DEFAULT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `national_id` varchar(45) DEFAULT NULL,
+  `level_id` int(11) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `contact_email` varchar(100) DEFAULT NULL,
+  `avatar` blob,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1',
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_user_id` int(11) NOT NULL,
+  `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_user_id` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`user_name`),
+  KEY `user_level_idx` (`level_id`),
+  KEY `user_sttatus_idx` (`status_id`),
+  CONSTRAINT `user_level` FOREIGN KEY (`level_id`) REFERENCES `gen_user_level` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user_status` FOREIGN KEY (`status_id`) REFERENCES `gen_user_status` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
+-- -------
+-- Rol
+-- -------
+CREATE TABLE `sec_rol` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) NOT NULL,
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `rol_user_idx` (`create_user_id`),
+  CONSTRAINT `rol_user` FOREIGN KEY (`create_user_id`) REFERENCES `gen_user` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- AUTO_INCREMENT de la tabla `users`
---
-ALTER TABLE `users`
-  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
-COMMIT;
+-- -------
+-- Privilegio
+-- -------
+CREATE TABLE `sec_privilege` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) NOT NULL,
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `privilege_user_idx` (`create_user_id`),
+  CONSTRAINT `privilege_user` FOREIGN KEY (`create_user_id`) REFERENCES `gen_user` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- -------
+-- Rol Privilegio
+-- -------
+CREATE TABLE `sec_rol_privilege` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rol_id` int(11) NOT NULL,
+  `privilege_id` int(11) NOT NULL,
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Unique` (`rol_id`,`privilege_id`),
+  KEY `rol_privilege_privilege_idx` (`privilege_id`),
+  KEY `rol_privilege_create_user_idx` (`create_user_id`),
+  CONSTRAINT `rol_privilege_create_user` FOREIGN KEY (`create_user_id`) REFERENCES `gen_user` (`id`),
+  CONSTRAINT `rol_privilege_privilege` FOREIGN KEY (`privilege_id`) REFERENCES `sec_privilege` (`id`),
+  CONSTRAINT `rol_privilege_rol` FOREIGN KEY (`rol_id`) REFERENCES `sec_rol` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- -------
+-- Relacion usuario Rol
+-- -------
+CREATE TABLE `gen_user_rol` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `rol_id` int(11) NOT NULL,
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_rol_user_idx` (`user_id`),
+  KEY `user_rol_rol_idx` (`rol_id`),
+  KEY `user_rol_create_user_idx` (`create_user_id`),
+  CONSTRAINT `user_rol_create_user` FOREIGN KEY (`create_user_id`) REFERENCES `gen_user` (`id`),
+  CONSTRAINT `user_rol_rol` FOREIGN KEY (`rol_id`) REFERENCES `sec_rol` (`id`),
+  CONSTRAINT `user_rol_user` FOREIGN KEY (`user_id`) REFERENCES `gen_user` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
