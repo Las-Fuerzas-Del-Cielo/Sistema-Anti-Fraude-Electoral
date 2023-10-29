1f8979a9964d47a4f328198e546163424c335db8
arieldupuy90 <ariel_dupuy@hotmail.com>
2023-10-29 01:01:31 -0300

    mejoro la performance usando varios hilos para descargar con chunks

--- Fuentes_de_datos/descargador_de_telegramas/descargador.php
+++ Fuentes_de_datos/descargador_de_telegramas/descargador.php
@@ -1,49 +1,29 @@
 <?php
+    
+    ini_set('log_errors','On');
+    ini_set('display_errors','Off');
+    ini_set('error_reporting', E_ALL );
+    define('WP_DEBUG', false);
+    define('WP_DEBUG_LOG', true);
+    define('WP_DEBUG_DISPLAY', false);
+
     require_once("./mesas.php");
-    //0900601232X
-    @mkdir(__DIR__.'/images', 0777, true);
-    chmod(__DIR__.'/images', 0777);
+    require_once("./threadedCommand.php");
 
-    foreach ($mesas as $mesa) {
-        echo 'descargando: '.$mesa.'';
-        touch(__DIR__.'/images/'.$mesa.'.jpg');
-        $base64Data = descargar($mesa);
-        $image = base64_to_jpeg($base64Data, __DIR__.'/images/'.$mesa.'.jpg' );
+    $directory = __DIR__.'/images/';
+    @mkdir($directory, 0777, true);
+    chmod($directory, 0777);
+    $chunks = array_chunk($mesas,10, true);
+    $dateSetDateStart = new DateTime("now");
+    foreach($chunks as $chunk) {
+        $dateStart = new DateTime("now");
+        ThreadedCommand::execute($directory, $chunk);
+        $dateEnd = new DateTime("now");
+        $Partialdiff = (float) $dateEnd->diff($dateStart)->f;
+        print_r("Tiempo en microsegundos: ".$Partialdiff."\n");
+        
     }
-    echo 'fin';
+    $endDataSetDate = new DateTime("now");
+    echo 'fin, tiempo de ejecucion: ' . ($endDataSetDate->diff($dateSetDateStart)->format('H:i:s.u'));
     exit;
-
-    function descargar($mesa) {
-        $endpoint = "https://resultados.gob.ar/backend-difu/scope/data/getTiff/$mesa";
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://resultados.gob.ar/backend-difu/scope/data/getTiff/'.$mesa,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_HTTPHEADER => array(
-                'Accept: */*',
-                'Connection: keep-alive',
-                'Accept-Encoding: gzip, deflate, br',
-                'User-Agent: PostmanRuntime/7.33.0',
-              ),
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-        ));
-
-        $response = curl_exec($curl);
-        $responseParsed = json_decode($response);    
-        curl_close($curl);
-        return $responseParsed->encodingBinary;
-    }
-
-    function base64_to_jpeg( $base64_string, $output_file ) {
-        $status = file_put_contents($output_file, base64_decode($base64_string));
-        chmod($output_file,777);
-        return($status); 
-    }
-    
-    
 ?>
