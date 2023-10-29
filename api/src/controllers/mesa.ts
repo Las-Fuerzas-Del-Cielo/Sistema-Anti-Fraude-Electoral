b633355654f040ddfd89a00f7686d5a62c0a2a7b
Ale Chicala <alechicala@Ales-MacBook-Air.local>
2023-10-29 20:02:28 -0300

    Endpoint para obtener informacion de mesa segun id arreglado

--- api/src/controllers/mesa.ts
+++ api/src/controllers/mesa.ts
@@ -1,46 +1,31 @@
 import axios from 'axios'
 import { RequestHandler } from 'express'
+import { EleccionTipo, GetResultadosResponse, RecuentoTipo, ResultadosApi } from '../clients/resultadosApi'
 
 export const getMesaData: RequestHandler = async (req, res) => {
-  // Mocked Logic
 
   const {id: mesaId} = req.params;
+  const {anioEleccion, tipoRecuento, tipoEleccion, categoriaId, distritoId, circuitoId, seccionId, seccionProvincialId } = req.query;
 
-  const {anioEleccion, tipoRecuento, tipoEleccion, categoriaId, distritoId} = req.query;
-
-  if(!anioEleccion || !tipoRecuento || !tipoEleccion || !categoriaId || !distritoId){
-    return res.status(400).send("parametros obligatorios: anioEleccion, tipoRecuento, tipoEleccion, categoriaId, distritoId")
-  }
-
-
-  try {
-
-    const resp = await axios.get(`https://resultados.mininterior.gob.ar/api/resultados/getResultados`, { params: {
-      anioEleccion,
-      tipoRecuento,
-      tipoEleccion,
-      categoriaId,
-      distritoId,
-      mesaId 
-    } })
-  
-    const data = resp.data;
-
-    if(data == null || data == undefined) return res.status(400).send({ mesaData : "No data"});
-    if(!data.hasOwnProperty("estadoRecuento")) return res.status(400).send({ mesaData : "No data recuento"});
+  await new ResultadosApi().getResultados({
+    anioEleccion,
+    tipoRecuento,
+    tipoEleccion,
+    categoriaId,
+    distritoId,
+    circuitoId,
+    mesaId,
+    seccionId,
+    seccionProvincialId
+  })
+  .then( (response: GetResultadosResponse) => {
+    return res.status(200).json(response);
+  })
+  .catch(error => {
+      // La solicitud se completó, pero el servidor devolvió un código de estado distinto de 2xx (como 404, 500, etc.)
+      res.status(error.response.status).send(error.response.data);
     
-    const { cantidadElectores, cantidadVotantes } = data.estadoRecuento;
-    if(cantidadElectores == 0 && cantidadVotantes == 0 ) return res.status(400).send({ mesaData : "Mesa con 0 electores y 0 votantes"});
-    if(cantidadVotantes > cantidadElectores) return res.status(400).send({ fraude: "hay mayor cantidad de votantes que electores en esta mesa", mesaData : data});
-    
-
-
-    res.status(200).json({ mesaData: data })
-
-    
-  } catch (error) {
-    res.status(500).send(error);
-  }
+  });
 
 }
 
