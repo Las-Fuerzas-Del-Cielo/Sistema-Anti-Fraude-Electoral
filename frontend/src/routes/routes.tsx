7bdef1fe64d2189d9b2adee61de912d810173fbd
jeronimoek <jeronimoek@gmail.com>
2023-10-29 18:55:17 -0300

    feat: pagina de carga de certificado

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 
 const Login = lazy(() => import('#/pages/login/login'));
 const Profile = lazy(() => import('#/pages/profile/profile'));
+const UploadCertificate = lazy(() => import('#/pages/upload-certificate/uploadCertificate'));
 const SendSuccess = lazy(() => import('#/pages/send-success/sendSuccess'));
 
 const AppRoutes: React.FC = () => (
   <Routes>
     <Route path='/login' element={<Login />} />
     <Route path='/profile' element={<Profile />} />
+    <Route path='/upload-certificate' element={<UploadCertificate />} />
     <Route path='/send-success' element={<SendSuccess />} />
     <Route path='/' element={<Login />} />
   </Routes>
