5b38adc2d86c4c1bcd1ed0d23d4029de193c9e3a
bonino97 <juanbonino97@gmail.com>
2023-10-29 08:30:05 +0100

    Add coment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   return (
+    // TODO: Agregar un spinner de carga o algun mensaje mientras se carga la app.
     <Suspense fallback={<div>Cargando...</div>}>
       <AppRoutes />
     </Suspense>
