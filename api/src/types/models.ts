49c68422fc8cf919e2ccfe6882629546646b2838
Ale Ponzo <aleponzo1@gmail.com>
2023-10-29 14:31:44 -0300

    Agrego interfaz de Mesa, Escuela y ReporteFaltaFiscal

--- api/src/types/models.ts
+++ api/src/types/models.ts
@@ -15,3 +15,27 @@ export interface Fiscalizar {
 export interface Session {
   userId: string
 }
+
+// Interfaz para representar una Mesa
+export interface Mesa {
+  id: string;
+  numero: number;
+  escuelaId: string; // Referencia a la escuela asociada
+}
+
+// Interfaz para representar una Escuela
+export interface Escuela {
+  id: string;
+  nombre: string;
+  direccion: string;
+}
+
+// Interfaz para el reporte de falta de fiscales
+export interface ReporteFaltaFiscal {
+  id: string; // ID único para el reporte
+  fiscalId: string; // ID del fiscal que reporta
+  mesaId: string; // ID de la mesa donde falta el fiscal
+  escuelaId: string; // ID de la escuela asociada a la mesa
+  timestamp: Date; // Fecha y hora del reporte
+  observaciones: string; // Observaciones adicionales si son necesarias
+}
