2eefd622515420011095294da9ab0b516cdc9a4f
Ale Chicala <alechicala@Ales-MacBook-Air.local>
2023-10-29 16:21:29 -0300

    Endpoint para obtener informacion de mesa segun id

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,12 +4,16 @@ import mesaRoutes from './routes/mesa'
 import denunciaRoutes from './routes/denuncia'
 import fiscalizarRoutes from './routes/fiscalizar'
 import { bootstrap } from './/bootstrap'
+import express from 'express';
 
 const app = bootstrap()
+app.use( express.json() );
 
 app.use('/api', userRoutes)
 app.use('/api', mesaRoutes)
 app.use('/api', denunciaRoutes)
 app.use('/api', fiscalizarRoutes)
+
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server up and running. \nSwagger UI running at http://localhost:${port}/api-docs`))
