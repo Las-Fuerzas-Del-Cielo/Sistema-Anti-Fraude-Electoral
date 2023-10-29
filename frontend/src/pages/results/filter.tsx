8a6c84994fe113366340f8abe6f1723a933e3fc4
Ludmila Molina <ludmila.molina@tunubi.com>
2023-10-29 19:57:44 -0300

    ✅ Create filters screen page

---
+++ frontend/src/pages/results/filter.tsx
@@ -0,0 +1,39 @@
+import { Selector } from "#/components/selector";
+import Button from "#/components/button";
+import { observer } from "mobx-react-lite";
+import Navbar from "#/components/navbar";
+
+const FilterPage = () => {
+  const DummyData = ["Example", "Example 1", "Example 2"];
+
+  return (
+    <>
+    <Navbar />
+    <main className="items-center flex flex-col relative px-10">
+      <section className="md:w-1/2 w-full rounded-xl z-10 mt-10">
+        <Selector provincias={DummyData} placeholder="Distrito" />
+        <Selector provincias={DummyData} placeholder="Sección Electoral" />
+        <Selector provincias={DummyData} placeholder="Sección" />
+        <Selector provincias={DummyData} placeholder="Municipio" />
+        <Selector provincias={DummyData} placeholder="Circuito " />
+        <Selector provincias={DummyData} placeholder="Establecimiento" />
+        <Selector provincias={DummyData} placeholder="Mesa" />
+        <Button
+          className="bg-violet-brand p-4 text-white w-full rounded-xl font-semibold text-xl tracking-wider"
+          type="submit"
+          label="Aplicar Filtros"
+          />
+        <Button
+            className="border-2 border-red text-red bg-transparent p-3 w-full rounded-xl text-xl tracking-wider shadow-md hover:border-violet-light mt-3"
+            type="submit"
+            label="Alertar Irregularidades"
+          />
+      </section>
+    </main>
+    </>
+  );
+};
+
+export const Filter = observer(FilterPage);
+
+export default Filter;
--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -5,6 +5,7 @@ const Login = lazy(() => import("#/pages/login/login"));
 const Profile = lazy(() => import("#/pages/profile/profile"));
 const SendSuccess = lazy(() => import("#/pages/send-success/sendSuccess"));
 const Dashboard = lazy(() => import("#/pages/dashboard/dashboard"));
+const FilterPage = lazy(() => import("#/pages/results/filter"));
 
 const AppRoutes: React.FC = () => (
   <Routes>
@@ -13,6 +14,7 @@ const AppRoutes: React.FC = () => (
     <Route path="/profile" element={<Profile />} />
     <Route path="/send-success" element={<SendSuccess />} />
     <Route path="/" element={<Login />} />
+    <Route path="/results" element={<FilterPage />} />
   </Routes>
 );
 
