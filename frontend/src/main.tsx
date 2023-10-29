e4a5fc81d657e8995facc8244618fab91f14be02
luciano-repetti <lucianorepetti2020@gmail.com>
2023-10-29 03:57:41 -0300

    new screen profile

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 import './custom.css';
 import { StoreProvider } from './store/index.js';
