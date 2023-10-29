c60a3006d621d61c19060d723473d2976876f21a
Ale Ponzo <aleponzo1@gmail.com>
2023-10-29 15:01:31 -0300

    Traducidos los errores al ingles

--- api/src/utils/errorConstants.ts
+++ api/src/utils/errorConstants.ts
@@ -1,32 +1,31 @@
 export const ERROR_CODES = {
     INCOMPLETE_DATA: {
       status: 400,
-      message: 'Datos incompletos. Por favor, complete todos los campos requeridos.'
+      message: 'Incomplete data. Please fill in all required fields.'
     },
     UNAUTHORIZED_GENERAL: {
       status: 403,
-      message: 'Acceso denegado. Se requiere ser fiscal general para realizar esta acción.'
+      message: 'Access denied. General fiscal role required for this action.'
     },
     RESOURCE_NOT_FOUND: {
       status: 404,
-      message: 'Recurso no encontrado. Verifique los datos proporcionados.'
+      message: 'Resource not found. Please verify the provided data.'
     },
     INSTITUTION_NOT_FOUND: {
         status: 404,
-        message: "Institución del fiscal no encontrada."
+        message: "Fiscal's institution not found."
     },
     INVALID_MESA_OR_ESCUELA: {
       status: 404,
-      message: 'Mesa o escuela no válida. Verifique que la mesa pertenezca a la escuela indicada.'
+      message: 'Invalid mesa or school. Verify that the mesa belongs to the indicated school.'
     },
     INTERNAL_SERVER_ERROR: {
       status: 500,
-      message: 'Error interno del servidor. Inténtelo de nuevo más tarde.'
+      message: 'Internal server error. Please try again later.'
     },
     S3_UPLOAD_ERROR: {
         status: 500,
-        message: "Error al registrar el reporte en S3."
+        message: "Error registering the report in S3."
     },
-    // Agregar más errores según sea necesario
-  };
-  
+    // Add more errors as needed
+};
