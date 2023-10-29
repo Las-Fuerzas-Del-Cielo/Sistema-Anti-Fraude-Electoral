1a3e1a9f7bb729d97fd78109dd13c8d78a6f6cfe
Matias Izquierdo <izquierdiito@gmail.com>
2023-10-29 18:16:00 -0300

    chore: added typified consts and changed to dashboard page

--- frontend/src/pages/dashboard/dashboard.tsx
+++ frontend/src/pages/dashboard/dashboard.tsx
@@ -2,17 +2,19 @@ import Button from "#/components/button";
 import Navbar from "#/components/navbar";
 import { observer } from "mobx-react-lite";
 
-const DashboardContent = () => {
+const DashboardPage = () => {
+  const user: string = "Javier Gerardo";
+  const email: string = "Javo@gmail.com";
+  const href: string = "#";
+
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar />
       <section className="flex justify-center">
         <div className="md:w-1/2 w-5/6 shadow-3xl rounded-xl p-8 flex flex-col items-center space-y-8">
           <div className="container mx-auto flex flex-col items-center text-gray bg-white rounded-xl p-2 text-gray-dark shadow-md mb-12">
-            <h2 className="text-xl font-bold mb-2">
-              ¡Bienvenido Javier Gerarddo!
-            </h2>
-            <p className="text-gray-500">Javo@gmail.com</p>
+            <h2 className="text-xl font-bold mb-2">¡Bienvenido {user}!</h2>
+            <p className="text-gray-500">{email}</p>
           </div>
           <div className="flex flex-col items-center space-y-8 w-full">
             <Button
@@ -33,7 +35,7 @@ const DashboardContent = () => {
             <div className="text-center text-gray-600 text-md mt-8">
               ¿No funciona el Formulario? <br />
               Realiza la denuncia{" "}
-              <a href="#" className="text-violet-brand underline">
+              <a href={href} className="text-violet-brand underline">
                 aquí
               </a>
             </div>
@@ -44,6 +46,6 @@ const DashboardContent = () => {
   );
 };
 
-export const Dashboard = observer(DashboardContent);
+export const Dashboard = observer(DashboardPage);
 
 export default Dashboard;
