52f6a0bae8540583ec856ddbf88625be3a1d2faf
KevOrtega <kevortega.dev@gmail.com>
2023-10-29 00:14:12 -0300

    feat(api/main): swagger added and basic config

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Anti Electoral Fraud API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('Anti Electoral Fraud')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
