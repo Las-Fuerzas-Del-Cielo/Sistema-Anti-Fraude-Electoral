d30c4a742a611b18d0a35d4fa528b6c58efda3a2
luciano-repetti <lucianorepetti2020@gmail.com>
2023-10-29 03:17:19 -0300

    creation screen profile

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './custom.css';
 import { StoreProvider } from './store/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
