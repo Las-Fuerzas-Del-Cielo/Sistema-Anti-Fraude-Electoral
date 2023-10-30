8c7a6a4e315c8cc64d281098e85efc7fb1c1c18a
Juan Turk <juan.turk@simplisafe.com>
2023-10-30 12:35:42 -0300

    Improve config loading

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,10 @@
-require('dotenv').config()
-
-import { bootstrap } from './bootstrap'
-import routes from "./routes";
+import { bootstrap } from './bootstrap';
+import routes from './routes';
+import config from './config';
 
 const app = bootstrap()
+const { port } = config
 
 routes.forEach(({ prefix, router }) => app.use(prefix, router));
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Server up and running. \nSwagger UI running at http://localhost:${port}/api-docs`))
+app.listen(port, () => console.log(`Server up and running. \nSwagger UI running at http://localhost:${config}/api-docs`))
