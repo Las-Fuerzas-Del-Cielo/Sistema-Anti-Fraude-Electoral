bd908aea4a4972c246c804dc7e1a3f7e91fdf8f8
Ale Ponzo <aleponzo1@gmail.com>
2023-10-30 14:55:23 -0300

    [API] Reporte de falta de fiscales (#113)
    * Commit inicial para reporte de falta de fiscales
    
    * Agrego interfaz de Mesa, Escuela y ReporteFaltaFiscal
    
    * Fixes basados en Code Review
    
    * Traducidos los errores al ingles
    
    * Agregado tipo de ResultadoRegistroS3 y traducciones
    
    * Correccion comentario
    
    * Agrego validacion de error en ResultadoRegistroS3 y agrego tipos
    
    * Change to english certain variables and methods
    
    * Add type to resultadoS3
    
    * Correct reference
    
    * Add and create specific types
    
    * Correct types
    
    * Add missing types

---
+++ api/src/utils/errorConstants.ts
@@ -0,0 +1,36 @@
+type ErrorCode = {
+    status: number;
+    message: string;
+};
+
+export const ERROR_CODES: Record<string, ErrorCode> = {
+    INCOMPLETE_DATA: {
+      status: 400,
+      message: 'Incomplete data. Please fill in all required fields.'
+    },
+    UNAUTHORIZED_GENERAL: {
+      status: 403,
+      message: 'Access denied. General fiscal role required for this action.'
+    },
+    RESOURCE_NOT_FOUND: {
+      status: 404,
+      message: 'Resource not found. Please verify the provided data.'
+    },
+    INSTITUTION_NOT_FOUND: {
+        status: 404,
+        message: "Fiscal's institution not found."
+    },
+    INVALID_MESA_OR_ESCUELA: {
+      status: 404,
+      message: 'Invalid mesa or school. Verify that the mesa belongs to the indicated school.'
+    },
+    INTERNAL_SERVER_ERROR: {
+      status: 500,
+      message: 'Internal server error. Please try again later.'
+    },
+    S3_UPLOAD_ERROR: {
+        status: 500,
+        message: "Error registering the report in S3."
+    },
+    // Add more errors as needed
+};
