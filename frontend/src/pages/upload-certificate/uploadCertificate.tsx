5d2200e21769bad369b6a382757f68c499e26922
bonino97 <juanbonino97@gmail.com>
2023-10-30 05:09:33 +0100

    Fix icon check

--- frontend/src/pages/upload-certificate/uploadCertificate.tsx
+++ frontend/src/pages/upload-certificate/uploadCertificate.tsx
@@ -11,7 +11,7 @@ import './styles.css';
 const CheckItem = ({ text }: { text: string }) => (
   <div className="flex justify-space-around items-center gap-2 h-12">
     <div className="flex justify-center items-center rounded-full bg-green-check text-white w-5 h-5 flex-shrink-0">
-      <img className="w-3 h-3" src="src/assets/check-icon.svg" alt="" />
+      <img className="w-3 h-3" src="src/assets/icon/check-icon.svg" alt="" />
     </div>
     <p>{text}</p>
   </div>
