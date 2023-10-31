784b81869558bb03954d3b91369692be21818e85
daxthin <nicolasdga.dev@gmail.com>
2023-10-30 21:23:30 -0300

    footer decor fixed

--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -78,7 +78,13 @@ const LoginPage: React.FC = () => {
         </form>
       </div>
 
-      <div className="absolute left-0 right-0 transform -skew-y-12 -bottom-32 h-80 bg-violet-brand"></div>
+      <div 
+        className="absolute left-0 right-0 bottom-0 h-screen bg-violet-brand" 
+        style={
+          {
+            clipPath: 'polygon(0 90%, 100% 80%, 100% 100%, 0% 100%)'
+          }
+      }/>
 
       {/* 
         // TODO: FIX FOOTER IMAGE DESIGN 
